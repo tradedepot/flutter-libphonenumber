071e1f99cd69baf8a00f588582439515867f9798
Tomisin-E <lilboythomson@gmail.com>
2024-09-03 16:19:17 +0100

    update

--- ios/Classes/LibphonenumberPlugin.m
+++ ios/Classes/LibphonenumberPlugin.m
@@ -13,7 +13,7 @@
                                                                 binaryMessenger:[registrar messenger]];
     
     LibphonenumberPlugin* instance = [[LibphonenumberPlugin alloc] init];
-    instance.phoneUtil = [[NBPhoneNumberUtil alloc] init];
+    // instance.phoneUtil = [[NBPhoneNumberUtil alloc] init];
     
     [registrar addMethodCallDelegate:instance channel:channel];
 }
